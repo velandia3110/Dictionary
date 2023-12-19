983e76146e671b9a10778e413584152e1e628382
Juan Pablo <duenasjuanpablo1939@gmail.com>
2023-12-19 02:35:20 -0500

    Primer adelanto de interfaz

--- src/view/gui/JFMain.java
+++ src/view/gui/JFMain.java
@@ -1,5 +1,9 @@
 package view.gui;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,19 +11,47 @@ import javax.swing.JPanel;
 
 public class JFMain extends JFrame{
 	
-	JPanel pnlHead;
-	JPanel pnlBody;
-
+	private JPanel pnlBackground;
+	private JPanel pnlBody;
+	private JLabel lblTitle;
+	private JButton btnAdd;
+	private JButton btnSearch;
+	private JButton btnModify;
+	private JButton btnGetChar;
+	private JButton btnGetList;
+	
 	
 	public JFMain() {
-			
-			setSize(515,620);
+					
+			setSize(515,400);
 			setLayout(null);
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 			setResizable(false);
+			this.getContentPane().setBackground(Color.white);
 			
+			lblTitle = new JLabel();
+			pnlBackground = new JPanel();
 			
+			createHead();
+			createBackground();
+			
+			add(lblTitle);
+			add(pnlBackground);
 			
 			setVisible(true);
+	}
+
+
+	private void createBackground() {
+		pnlBackground.setBounds(0, 230, 515, 120);
+		pnlBackground.setBackground(Color.decode("#0070C0"));
+		
+	}
+
+
+	private void createHead() {
+		lblTitle.setBounds(0,0,500,117);
+		lblTitle.setBackground(Color.black);
+		lblTitle.setIcon(new ImageIcon("Resource/TilteDictionary.png"));	
 	}
 }
