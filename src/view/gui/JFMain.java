48c8b164f3d75410a6bf0364c2df031d45fabca6
Julian <julianvelandia3110@gmail.com>
2023-12-19 08:17:32 -0500

    Se añadio botones a JFMain

--- src/view/gui/JFMain.java
+++ src/view/gui/JFMain.java
@@ -31,12 +31,28 @@ public class JFMain extends JFrame{
 			
 			lblTitle = new JLabel();
 			pnlBackground = new JPanel();
+			pnlBody = new JPanel();
+			
+			btnAdd = new JButton("Añadir palabra");
+			btnSearch = new JButton("Buscar palabra");
+			btnModify = new JButton("Editar palabra");
+			btnGetList = new JButton("Listar diccionario");
+			btnGetChar = new JButton("Listar por letra");
 			
 			createHead();
 			createBackground();
+			createBody();
 			
 			add(lblTitle);
 			add(pnlBackground);
+			add(pnlBody);
+			
+			
+			pnlBody.add(btnAdd);
+			pnlBody.add(btnSearch);
+			pnlBody.add(btnModify);
+			pnlBody.add(btnGetList);
+			pnlBody.add(btnGetChar);
 			
 			setVisible(true);
 	}
@@ -46,8 +62,17 @@ public class JFMain extends JFrame{
 		pnlBackground.setBounds(0, 230, 515, 120);
 		pnlBackground.setBackground(Color.decode("#0070C0"));
 		
+		
 	}
 
+	private void createBody() {
+		pnlBody.setBounds(0, 100, 500, 163);
+		btnAdd.setBounds(100, 50, 200, 50);
+		btnSearch.setBounds(100, 120, 200, 50);
+		btnModify.setBounds(100, 190, 200, 50);
+		btnGetList.setBounds(100, 260, 200, 50);
+		btnGetChar.setBounds(100, 330, 200, 50);
+	}
 
 	private void createHead() {
 		lblTitle.setBounds(0,0,500,117);
