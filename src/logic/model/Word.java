78759996820998d77d040b505141f5b39aef51d2
Juan Pablo <100857122+JuanDuenas@users.noreply.github.com>
2023-12-18 23:20:59 -0500

    Agregada Estructura de Dictionary

---
+++ src/logic/model/Word.java
@@ -0,0 +1,19 @@
+package logic.model;
+
+public class Word {
+	private String word;
+	
+	public Word(String word) {
+		this.word = word;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+	
+}
