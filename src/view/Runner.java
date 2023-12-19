7d7296ea0d16b45f95459e0dabac28989b3938f6
Juan Pablo <duenasjuanpablo1939@gmail.com>
2023-12-19 01:09:48 -0500

    Borrado el test

---
+++ src/view/Runner.java
@@ -0,0 +1,8 @@
+package view;
+import view.gui.*;
+
+public class Runner {
+	public static void main(String[]args) {
+		new JFMain();
+	}
+}
---
+++ src/view/gui/JFMain.java
@@ -0,0 +1,25 @@
+package view.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class JFMain extends JFrame{
+	
+	JPanel pnlHead;
+	JPanel pnlBody;
+
+	
+	public JFMain() {
+			
+			setSize(515,620);
+			setLayout(null);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			setResizable(false);
+			
+			
+			
+			setVisible(true);
+	}
+}
